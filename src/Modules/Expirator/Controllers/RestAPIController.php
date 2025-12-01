71829fed7b1db17c93957ae5e52800951c7e614f
ojopaul <ojopaul.dev@gmail.com>
2025-12-01 06:16:12 +0100

    - Enhance post expiration extraData data validation

--- src/Modules/Expirator/Controllers/RestAPIController.php
+++ src/Modules/Expirator/Controllers/RestAPIController.php
@@ -107,6 +107,16 @@ class RestAPIController implements InitializableInterface
                     'required' => true,
                     'type' => 'integer',
                 ],
+                'extraData' => [
+                    'validate_callback' => function ($param, $request, $key) {
+                        return is_array($param);
+                    },
+                    'sanitize_callback' => function ($param, $request, $key) {
+                        return $this->sanitizeExtraData($param);
+                    },
+                    'required' => false,
+                    'type' => 'array',
+                ],
             ]
         ]);
 
@@ -489,7 +499,7 @@ class RestAPIController implements InitializableInterface
 
             $extraData = $request->get_param('extraData');
             if (is_array($extraData) && !empty($extraData)) {
-                $opts['extraData'] = $extraData;
+                $opts['extraData'] = $this->sanitizeExtraData($extraData);
             }
 
             $this->hooks->doAction(
@@ -503,5 +513,26 @@ class RestAPIController implements InitializableInterface
         }
 
         return rest_ensure_response(true);
+    }
+
+    private function sanitizeExtraData($data)
+    {
+        if (!is_array($data)) {
+            if (is_string($data)) {
+                return sanitize_text_field($data);
+            }
+            if (is_numeric($data) || is_bool($data)) {
+                return $data;
+            }
+            return null;
+        }
+
+        $sanitized = [];
+        foreach ($data as $key => $value) {
+            $sanitizedKey = sanitize_key($key);
+            $sanitized[$sanitizedKey] = $this->sanitizeExtraData($value);
+        }
+
+        return $sanitized;
     }
 }
